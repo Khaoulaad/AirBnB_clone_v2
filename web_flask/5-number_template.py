51fda1387d39a090dfe21793ac590fb7cb72912a
Khaoulaad <ouajaa.khaoula.com>
2023-12-27 06:21:00 -0800

    0

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Starts a Flask web app """
+""" Starts a Flask web appli """
 from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
