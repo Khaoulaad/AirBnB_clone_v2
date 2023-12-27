a4050c36f80f3ef181a386f886341e34b5bc52db
Khaoulaad <ouajaa.khaoula.com>
2023-12-27 05:36:41 -0800

    0

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,39 +1,36 @@
 #!/usr/bin/python3
-"""  Add third vie func that redirects"""
-
+""" Starts a Flask web application """
 from flask import Flask
-
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello_world():
-    """ Returns some text. """
+def hello_hbnb():
+    """ Hello Hbnb"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
-def hello():
-    """ Return other text. """
+def hbnb():
+    """ Hello hbnb """
     return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c_text(text):
-    """ replace text with variable. """
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+def c_compliment(text):
+    """ Displaying a message starting with C """
+    msg = text.replace('_', ' ')
+    return 'C %s' % msg
 
 
 @app.route('/python/')
 @app.route('/python/<text>')
-def python_text(text='is cool'):
-    """ replace more text with another variable. """
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
+def python_compliment(text='is_cool'):
+    """ Displaying a message starting with Python """
+    msg = text.replace('_', ' ')
+    return 'Python %s' % msg
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)i
+    app.run(host='0.0.0.0', port=5000)
