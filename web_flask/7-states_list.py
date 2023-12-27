9ccbaf85548ae4bf853baafd5b566e786fc22984
Khaoulaad <ouajaa.khaoula.com>
2023-12-27 08:08:29 -0800

    0

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
-""" Starting a Flask web app """
-from flask import Flask, render_template
+"""
+A script that starts a Flask web application:
+"""
+
+from flask import Flask
 from models import storage
-from models.state import State
+from flask import render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list_route():
+    """
+    List states: display a HTML page: (inside the tag BODY)
+    Returns:
+        html: template that lists all states sort by name A->Z
+    """
+    states = storage.all("State").values()
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def dispose(exception):
-    """ Removing current session """
+def close_db(exception=None):
+    """
+    After each request remove the current SQLAlchemy Session:
+    """
     storage.close()
 
 
-@app.route('/states_list')
-def states():
-    """ Displaying list of all the states """
-    states = storage.all(State)
-    statesList = list(states.values())
-    return render_template('7-states_list.html', states=statesList)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
